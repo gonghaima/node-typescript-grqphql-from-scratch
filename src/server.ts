092b7aa766e1736caaec779395093f050bb3bcee
Steven <gonghaima@hotmail.com>
2020-10-20 20:44:51 +1100

    fastify

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,14 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+const { ApolloServer, gql } = require('apollo-server-fastify');
+
 import depthLimit from 'graphql-depth-limit';
-import { createServer } from 'http';
-import compression from 'compression';
-import cors from 'cors';
 import schema from './schema';
-const app = express();
+const app = require('fastify')();
 const server = new ApolloServer({
     schema,
     validationRules: [depthLimit(7)],
 });
-app.use('*', cors());
-app.use(compression());
-server.applyMiddleware({ app, path: '/graphql' });
-const httpServer = createServer(app);
-httpServer.listen(
-    { port: 3000 },
-    (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
+
+(async function () {
+    app.register(server.createHandler());
+    await app.listen(3000);
+})();
